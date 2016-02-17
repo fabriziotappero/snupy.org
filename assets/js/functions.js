44e886b895287ef2cfe19ea59a39eb9bacdc0e67
Fabrizio <fabrizio.tappero@gmail.com>
2016-02-17 12:19:34 +0100

    more client oriented content

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,9 +5,9 @@ $(function() {
 	clientStuff();
 
 
-	$("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
-	$(".biglink").fitText(1.5);
-	
+	//$("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
+	//$(".biglink").fitText(1.5);
+
 });
 
 // smoothScroll function is applied from the document ready function
@@ -27,7 +27,7 @@ function smoothScroll (duration) {
 
 
 function workBelt() {
-  
+
   $(".trigger").remove();
   $(".return").remove();
 
@@ -38,7 +38,7 @@ function workBelt() {
     $('html, body').animate({scrollTop: $('#work').offset().top},300);
 
   });
-  
+
   $('.work-return').click(function() {
     //$('.work-belt').removeClass("slided");
     $('.work-container').hide(800);
@@ -49,16 +49,16 @@ function workBelt() {
 
 
 function  workLoad() {
-  
+
   $.ajaxSetup({ cache: true });
-  
+
   $('.thumb-unit').click(function() {
     var $this = $(this),
         newTitle = $this.find('strong').text(),
         newfolder = $this.data('folder'),
         spinner = '<div class="loader">Loading...</div>',
         newHTML = 'work/'+ newfolder;
-   
+
     // load new page content and, when completed, run the vimeo iframe resize window
     $('.project-load').html(spinner).load(newHTML,function(){$('.project-load').fitVids();});
     $('.project-title').text(newTitle);
@@ -70,52 +70,52 @@ function  workLoad() {
 
 
 function clientStuff() {
-  
+
   $('.client-unit').first().addClass('active-client');
   $('.client-logo').first().addClass('active-client');
   $('.clients-mobile-nav span').first().addClass('active-client');
-  
+
   $('.client-logo, .clients-mobile-nav span').click(function() {
     var $this = $(this),
         $siblings = $this.parent().children(),
         position = $siblings.index($this);
-        
+
     $('.client-unit').removeClass('active-client').eq(position).addClass('active-client');
     $siblings.removeClass('active-client');
     $this.addClass('active-client');
   });
-  
-  
+
+
   $('.client-control-next, .client-control-prev').click(function() {
-  
+
     var $this = $(this),
         curActiveClient = $('.clients-belt').find('.active-client'),
         position = $('.clients-belt').children().index(curActiveClient),
         clientNum = $('.client-unit').length;
-        
+
       if($this.hasClass('client-control-next')) {
-        
+
         if(position < clientNum -1){
           $('.active-client').removeClass('active-client').next().addClass('active-client');
         } else {
           $('.client-unit').removeClass('active-client').first().addClass('active-client');
           $('.client-logo').removeClass('active-client').first().addClass('active-client');
         }
-        
+
       } else {
-        
+
         if (position === 0) {
           $('.client-unit').removeClass('active-client').last().addClass('active-client');
           $('.client-logo').removeClass('active-client').last().addClass('active-client');
         } else {
-          $('.active-client').removeClass('active-client').prev().addClass('active-client');  
+          $('.active-client').removeClass('active-client').prev().addClass('active-client');
         }
 
       }
-        
-  
+
+
   });
-  
+
 }
 
 
@@ -151,13 +151,3 @@ function clientStuff() {
   };
 
 })( jQuery );
-
-
-
-
-
-
-
-
-
-
